1e9743ae2a6d0ae1a7d42559c789561288382a5d
vhtruong752 <vhtruong752@gmail.com>
2023-05-02 11:50:41 -0400

    Upload Friends & Inbox fxml

--- SocialMedia/src/main/java/com/socialmedia/Friends.java
+++ SocialMedia/src/main/java/com/socialmedia/Friends.java
@@ -1,4 +1,12 @@
 package com.socialmedia;
 
+import java.net.URL;
+import javafx.scene.control.ListView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
 public class Friends {
+
+    private ListView<AnchorPane> friendsList;
+
 }
