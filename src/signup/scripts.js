16ee113d9c1c474167b26d8612330a1e0e4f939e
jand6793 <jand6703@kettering.edu>
2023-05-29 21:12:30 -0400

    added missing login call

--- src/signup/scripts.js
+++ src/signup/scripts.js
@@ -37,6 +37,8 @@ document
                 return response.json();
             })
             .then((responseJson) => {
+                login(username.value, password.value);
+
                 // Clear the form
                 fullName.value = "";
                 username.value = "";
@@ -45,9 +47,6 @@ document
 
                 // Handle the response
                 console.log(responseJson);
-
-                // Redirect to the conversations page
-                window.location.href = "../conversations/page.html";
             })
             .catch((error) => {
                 // Handle the error
@@ -71,3 +70,68 @@ document
                     .appendChild(messageElement);
             });
     });
+
+function login(username, password) {
+    const formData = new URLSearchParams();
+    formData.append("grant_type", "");
+    formData.append("username", username);
+    formData.append("password", password);
+    formData.append("scope", "");
+    formData.append("client_id", "");
+    formData.append("client_secret", "");
+
+    // Now you can send 'username' and 'password' to your server
+    fetch("http://localhost:8000/api/v1/token", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            accept: "application/json",
+        },
+        body: formData,
+    })
+        .then((response) => {
+            if (response.ok) {
+                return response.json();
+            } else {
+                throw new Error("Error: " + response.statusText);
+            }
+        })
+        .then((responseJson) => {
+            // Store the received access token
+            localStorage.setItem("access_token", responseJson.access_token);
+            // Fetch the current user's details
+            fetch("http://localhost:8000/api/v1/users/me", {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    accept: "application/json",
+                    Authorization: `Bearer ${responseJson.access_token}`,
+                },
+            })
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error("Error: " + response.statusText);
+                    }
+                })
+                .then((responseJson) => {
+                    // Store the current user's ID
+                    localStorage.setItem("current_user", responseJson.id);
+
+                    // Handle the response
+                    console.log(responseJson);
+
+                    // Redirect to conversations page after successful login
+                    window.location.href = "../conversations/page.html";
+                })
+                .catch((error) => {
+                    // Handle the error
+                    console.error("Error:", error);
+                });
+        })
+        .catch((error) => {
+            // Handle the error
+            console.error("Error:", error);
+        });
+}
